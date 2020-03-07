3cc73b32c4d9ac69316b15d90551c037520b9b3b
bhavesh01shukla <shuklabhavesh01@gmail.com>
2020-03-07 21:17:37 +0530

    correct algo

--- task1.py
+++ task1.py
@@ -2,6 +2,8 @@ import numpy as np
 
 u=np.zeros((5,4,3))
 v=np.zeros((5,4,3))
+dif=np.zeros((5,4,3))
+
 
 gamma=0.99
 delta=0.001
@@ -9,116 +11,63 @@ final=100000000000000
 itr=0
 
 while(final > delta):
+    
     print('----------------------------------------------------------------------')
     print("ITERATION NO:",itr)
-    delta_list=[]
     for i in range(0,5):
         for j in range(0,4):
             for k in range(0,3):
-                
-                
                 ## i=> enemy health , j=> num arrows , k=> stamina
+                step_cost=5 
 
-                elem=[]
-                if i==0:
-                    policy='-1'
-                    reward= 0
-                    v[i,j,k]=reward + 0*u[i,j,k]
-                    print('(',i,j,k,'):',policy,v[i,j,k])
+                shoot=-100000000
+                if j>0 and k>0:
+                    if (i-1==0):
+                        reward=0.5*(10-step_cost) + 0.5*(-step_cost)
+                    else:
+                        reward=-step_cost
+                    shoot=reward + gamma*(0.5*u[i-1,j-1,k-1] + 0.5*u[i,j-1,k-1])
 
-                else:    
-                    ## action == shoot
-                    if j > 0 and k > 0:      ## can only shoot if no of arrows and stamina > 0
-                        t1=0.5 * u[i-1, j-1, k-1]
-                        t2=0.5 * u[i,j-1, k-1]
-
-                        if(i-1==0):
-                            reward=0.5*(10-20) + 0.5*(-20) ## enemy health == 0
-                        else:
-                            reward = -20
-                        elem.append([t1+t2,'SHOOT',reward])
-                        # elem.append([t2,'SHOOT',reward])
-
-                    ## action == recharge
-                    if k==2: ##stamina is full
-                        t1= 1 * u[i,j,k]
-                        reward= -1*20
-                        elem.append([t1,'RECHARGE',reward])                    
-                        
-                    else:   ### stamina is not full
-                        t1= 0.8 * u[i,j,k+1] ##recharged
-                        t2= 0.2 * u[i,j,k]   ## not recharged
-                        reward= 0.8*(-20) + 0.2*(-20)
-                        elem.append([t1+t2,'RECHARGE',reward])                    
-                        # elem.append([t2,'RECHARGE',reward])                                        
-                        
-                    ## action == dodge
-                    if k==1:
-                        if j==3:  ##arrows==3
-                            reward=-20
-                            t1= 1 * u[i,3,0]
-                            elem.append([t1,'DODGE',reward])                                           
-
-                        if j<3:  ##arrows<3
-                            t1= 0.2 * u[i,j,0]
-                            t2= 0.8 * u[i,j+1,0]
-                            reward= -20
-                            elem.append([t1+t2,'DODGE',reward])                                           
-                            # elem.append([t2,'DODGE',reward])                                           
-                                            
-                    elif k==2:
-                        if j==3:
-                            t1= 0.2 * u[i,3,0]
-                            t2= 0.8 * u[i,3,1]
-                            reward= -20
-                            elem.append([t1+t2,'DODGE',reward])                                           
-                            # elem.append([t2,'DODGE',reward])                                                                   
-
-                        elif j<3:
-                            t1= 0.2 * 0.2 * u[i,j,0]
-                            t2= 0.2 * 0.8 * u[i,j+1,0]
-                            t3= 0.8 * 0.2 * u[i,j,1]
-                            t4= 0.8 * 0.8 * u[i,j+1,1]
-                            reward= -20
-                            elem.append([t1+t2+t3+t4,'DODGE',reward])                                           
-                            # elem.append([t2,'DODGE',reward])                                           
-                            # elem.append([t3,'DODGE',reward])                                           
-                            # elem.append([t4,'DODGE',reward])                                           
-
-                    # print(i,j,k,elem)
-                    max=elem[0]
-
-                    for jj in elem:
-                        if jj[0]> max[0]:
-                            max=jj
-                    reward=max[2]
-                    policy=max[1]
-
-                    v[i,j,k]= max[0]
-                    v[i,j,k]*=gamma
-                    v[i,j,k]+=reward
-                    # print(v[i,j,k])
-                    temp=v[i,j,k]-u[i,j,k]
-                    if(temp<0):
-                        temp*=-1
-                    delta_list.append(temp)
-                    print('(',i,j,k,'):',policy,round(v[i,j,k],3))
-
+                recharge=-100000000
+                if k==2:
+                    reward=-step_cost
+                    recharge= reward + gamma*(1*u[i,j,k])
+                elif k<2:
+                    reward=-step_cost
+                    recharge=reward + gamma*(0.8*u[i,j,k+1]+0.2*u[i,j,k])
+                
+                dodge=-100000000
+                reward=-step_cost
+                if k==1:
+                    if j==3:
+                        dodge=reward + gamma*(u[i,3,0])
+                    elif j<3:
+                        dodge=reward + gamma*(0.2*u[i,j,0] + 0.8*u[i,j+1,0])
+                elif k==2:
+                    if j==3:
+                        dodge=reward + gamma*(0.2*u[i,3,0] + 0.8*u[i,3,1])
+                    elif j<3:
+                        dodge=reward + gamma*(0.2*0.2*u[i,j,0] + 0.2*0.8*u[i,j+1,0] + 0.8*0.2*u[i,j,1] + 0.8*0.8*u[i,j+1,1])
+                
+                temp=max(shoot,recharge,dodge)
+                
+                if i>0:
+                    if temp==shoot:
+                        print('(',i,j,k,')',v[i,j,k],'policy:shoot')
+                    elif temp==recharge:
+                        print('(',i,j,k,')',v[i,j,k],'policy:recharge')
+                    else:
+                        print('(',i,j,k,')',v[i,j,k],'policy:dodge')
+                    v[i,j,k]=temp
+                    
+                elif i==0:
+                    print('(',i,j,k,')',u[i,j,k],'policy:-1')
+                    v[i,j,k]=0
+                dif[i,j,k]=np.abs(v[i,j,k]-u[i,j,k])
+    
     u=np.copy(v)
+    final=np.max(dif)
+    print(final)
     itr+=1
-    # print(delta_list)
-    final=delta_list[0]
-    for bb in delta_list:
-        if bb > final:
-            final=bb
-    print('gamma',final)
-    # print(u)
-
-
-
-
-
-
-
-
+    
 
